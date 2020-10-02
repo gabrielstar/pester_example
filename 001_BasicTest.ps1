abd92855fed1dae655bd051bad75fea69a6fb26b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-02 16:24:50 +0200

    Pester simple tests

---
+++ 001_BasicTest.ps1
@@ -0,0 +1,17 @@
+Describe 'My tests' {
+    Context 'when it is 1' {
+        it 'should return this thing' {
+
+        }
+
+        it 'should not return that thing' {
+
+        }
+        it 'should call this method' {
+
+        }
+        it 'should create this file' {
+
+        }
+    }
+}
---
+++ 002_TestWithHooks.ps1
@@ -0,0 +1,41 @@
+Function CSVToJSON($filePathCSV, $filePathJSON)
+{
+    try
+    {
+        $csv = Get-Content -Path $filePathCSV -ErrorAction Stop
+        $csv = $csv | ConvertFrom-Csv | ConvertTo-Json | Out-File -Encoding UTF8 $filePathJSON
+    }
+    catch [System.Management.Automation.ItemNotFoundException]
+    {
+        "IO Error while rading/writing file: {0},{1}" -f $filePathCSV, $filePathJSON
+        "Terminating"
+    }
+    return
+
+}
+Describe 'File conversion tests' {
+    Context 'when I convert existing CSV file to JSON' {
+        BeforeAll {
+            CSVToJSON -filePathCSV "$PSScriptRoot/TestData/data.csv" `
+                        -filePathJSON "$PSScriptRoot/TestData/data.json"
+        }
+        AfterAll {
+            Remove-Item -Path "$PSScriptRoot/TestData/data.json"
+        }
+        it 'should JSON file exist' {
+            "$PSScriptRoot/TestData/data.json" | Should -Exist
+        }
+
+        it 'should JSON file be expected file' {
+            $actual = Get-Content  -Path "$PSScriptRoot/TestData/data.json"
+            $expected = Get-Content  -Path "$PSScriptRoot/TestData/data.json"
+            $expected | Should -Be $actual
+        }
+        it 'should not throw exception' {
+            {
+                CSVToJSON -filePathCSV "$PSScriptRoot/TestData/data.csv" `
+                        -filePathJSON "$PSScriptRoot/TestData/data.json"
+            } | Should -Not -Throw
+        }
+    }
+}
